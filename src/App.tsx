7686ba5c252288ceed4c8e41e9ebf6b3ad2ca4be
GusTiavo123 <luagus0@gmail.com>
2024-04-25 13:19:09 -0300

    Changing schema

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,23 @@ function App() {
     <Background>
       <header className="max-w-6xl w-full space-y-56">
         <Navbar />
-        <img src="computer.gif" alt="Descripción del GIF" className="cursor-pointer"/>
+        <div className="flex md:flex-row">
+          <div className="text-white">
+            <p>
+              Este es el texto que determina el tamaño de la sección. El texto
+              puede ser tan largo como sea necesario y la imagen se ajustará al
+              tamaño de esta sección de texto.
+            </p>
+          </div>
+          <div className=" bg-white m-0 p-0">
+            <img
+              src="computer.gif"
+              alt="Computer dance gif"
+              className="w-max h-auto object-cover"
+            />
+          </div>
+        </div>
+        <p>HOLAAAAAAAAAAAAAAA</p>
       </header>
     </Background>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,30 +1,19 @@
-import "./navbar.css";
-
 const Navbar = () => {
   return (
-      <nav className="flex justify-between items-center pt-2">
-        <div className="text-white font-bold text-2xl">Gustavo Aramayo</div>
-        <div>
-          <a
-            href="#"
-            className="text-white mr-5 hover:text-gray-300 cursor-pointer"
-          >
-            /Work
-          </a>
-          <a
-            href="/about"
-            className="text-white hover:text-gray-300 cursor-pointer"
-          >
-            /About
-          </a>
-          <a
-            href="/contact"
-            className="text-white ml-5 hover:text-red-600 cursor-pointer"
-          >
-            /Contact
-          </a>
-        </div>
-      </nav>
+    <nav className="flex justify-between items-center pt-2">
+      <div className="text-white font-bold text-2xl">Gustavo Aramayo</div>
+      <ul className="flex">
+        <li className="text-white text-lg mr-5 hover:text-gray-300 cursor-pointer">
+          <a href="/projects">/Projects</a>
+        </li>
+        <li className="text-white text-lg hover:text-gray-300 cursor-pointer">
+          <a href="/about">/About</a>
+        </li>
+        <li className="text-white text-lg ml-5 hover:text-gray-300 cursor-pointer">
+          <a href="/contact">/Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
