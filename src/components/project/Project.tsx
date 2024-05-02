d634d5db645c41563b56947bb2e3179d888b355b
GusTiavo123 <luagus0@gmail.com>
2024-05-02 13:59:53 -0300

    Finish project section

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,19 +1,15 @@
 import React, { useRef, useState } from "react";
-import FadeInWhenVisible from "../../effects/FadeInWhenVisible";
 import { SVGprops } from "../../types/types";
 import SVGIcon from "../SVGs/SVGIcon";
 
 type ProjectProps = {
   src: string;
-  alt: string;
   title: string;
   description: string;
-  technologies: string[];
+  technologies: string;
   liveUrl: string;
   repoUrl: string;
 };
-
-
 
 const Project: React.FC<ProjectProps> = ({
   src,
@@ -21,8 +17,10 @@ const Project: React.FC<ProjectProps> = ({
   description,
   technologies,
   liveUrl,
-  repoUrl
+  repoUrl,
 }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isHovered, setIsHovered] = useState(false);
 
   const svgs: SVGprops[] = [
     {
@@ -38,8 +36,6 @@ const Project: React.FC<ProjectProps> = ({
       label: "Code",
     },
   ];
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -56,57 +52,51 @@ const Project: React.FC<ProjectProps> = ({
   };
 
   return (
-    <FadeInWhenVisible>
-      <div className="flex w-full">
-        <div
-          className="relative flex flex-1 items-center justify-center"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <div className="h-auto w-full rounded-s-xl border-t-2 border-custom-box-border">
-            <video
-              ref={videoRef}
-              preload="metadata"
-              className="h-full w-full rounded-s-xl transition-opacity duration-300"
-              style={{ opacity: isHovered ? 1 : 0.7 }}
-              loop
-              muted
-            >
-              <source src={src} type="video/mp4" />
-              Your browser does not support videos.
-            </video>
-            {!isHovered && (
-              <div className="absolute inset-0 flex items-center justify-center">
-                <svg
-                  viewBox="0 0 24 24"
-                  className="h-14 w-14 text-white opacity-75"
-                >
-                  <path fill="currentColor" d="M8 5v14l11-7z"></path>
-                </svg>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-1 flex-col bg-custom-box-dark rounded-e-xl border-t-2 border-custom-box-border py-3 px-5">
-          <h3 className="text-2xl text-white font-semi-bold mb-2">{title}</h3>
-          <p className="text-custom-gray text-md mb-2">{description}</p>
-          <ul className="list-disc list-inside mb-2 text-white">
-            {technologies.map((tech) => (
-              <li key={tech}>{tech}</li>
-            ))}
-          </ul>
-          <div className="mt-auto">
-            <div className="flex justify-end">
-              {svgs.map((svg, index) => (
-                <div key={index} className="flex items-center m-2">
-                  <SVGIcon {...svg} />
-                </div>
-              ))}
+    <div className="flex w-full flex-col">
+      <div
+        className="relative flex items-center justify-center rounded-t-xl bg-custom-box-dark pt-3 px-3 border-t-2 border-custom-box-border"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="h-auto w-full">
+          <video
+            ref={videoRef}
+            preload="metadata"
+            className="w-full rounded-xl transition-opacity duration-300"
+            style={{ opacity: isHovered ? 1 : 0.7 }}
+            loop
+            muted
+          >
+            <source src={src} type="video/mp4" />
+            Your browser does not support videos.
+          </video>
+          {!isHovered && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <svg
+                viewBox="0 0 24 24"
+                className="h-14 w-14 text-white opacity-75"
+              >
+                <path fill="currentColor" d="M8 5v14l11-7z"></path>
+              </svg>
             </div>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col bg-custom-box-dark rounded-b-xl min-h-48 px-6 py-3">
+        <h3 className="text-2xl text-white font-semi-bold mb-2">{title}</h3>
+        <p className="text-custom-gray text-md mb-8">{description}</p>
+        <div className="flex justify-between mt-auto">
+          <p className="text-white italic">{technologies}</p>
+          <div className="flex">
+            {svgs.map((svg, index) => (
+              <div key={index} className="flex flex-row items-center mx-2">
+                <SVGIcon {...svg} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </FadeInWhenVisible>
+    </div>
   );
 };
 
--- src/sections/projects/Projects.tsx
+++ src/sections/projects/Projects.tsx
@@ -1,38 +1,56 @@
 import Project from "../../components/project/Project";
+import FadeInWhenVisible from "../../effects/FadeInWhenVisible";
 
 export default function Projects() {
   return (
     <section id="projects" className="max-w-7xl z-10 my-44">
       <h2 className="text-white text-3xl font-bold my-8">{"<Projects />"}</h2>
-      <div className="space-y-14">
-        <Project
-          src="project1.mp4"
-          alt="video project IdeaNova"
-          title="IdeaNova"
-          description="Website App for a fictional company featuring a contact form and modern login via NextAuth credentials. Authenticated users can access an internal blog to create and delete posts."
-          technologies={["React", "TailwindCSS", "Firebase"]}
-          liveUrl="https://idea-nova.vercel.app/"
-          repoUrl="https://github.com/GusTiavo123/IdeaNova"
-        />
-        <Project
-          src="project2.mp4"
-          alt="video project Nextxkcd"
-          title="Nextxkcd"
-          description="This is a brief description of Project 2."
-          technologies={["React", "TailwindCSS", "Firebase"]}
-          liveUrl="https://github.com/GusTiavo123/Comics-page"
-          repoUrl="https://github.com/GusTiavo123/Comics-page"
-        />
-        <Project
-          src="project3.mp4"
-          alt="video project MovieCloud"
-          title="MovieCloud"
-          description="This is a brief description of Project 3."
-          technologies={["Vue", "Vuetify", "Netlify"]}
-          liveUrl="https://github.com/GusTiavo123/moviecloud"
-          repoUrl="https://github.com/GusTiavo123/moviecloud"
-        />
-      </div>
+      <FadeInWhenVisible>
+        <div className="flex">
+          <div className="flex flex-1 mr-8 w-full">
+            <Project
+              src="project1.mp4"
+              title="IdeaNova"
+              description="Website App for a fictional company featuring a contact form and modern login via NextAuth credentials. Authenticated users can access an internal blog to create and delete posts."
+              technologies={"Next.js - CSS - HTML - MongoDB - NextAuth"}
+              liveUrl="https://idea-nova.vercel.app/"
+              repoUrl="https://github.com/GusTiavo123/IdeaNova"
+            />
+          </div>
+          <div className="flex flex-1 ml-8 w-full">
+            <Project
+              src="project2.mp4"
+              title="Nextxkcd"
+              description="Web application that provides a comic browsing experience, featuring a custom scraper for XKCD comics. Users can search comics, toggle between night and light modes for enhanced readability,"
+              technologies={"Next.js - TailwindCSS - ShadcnUI - Scraping"}
+              liveUrl="https://github.com/GusTiavo123/Comics-page"
+              repoUrl="https://github.com/GusTiavo123/Comics-page"
+            />
+          </div>
+        </div>
+      </FadeInWhenVisible>
+      <FadeInWhenVisible>
+        <div className="flex mt-16">
+          <div className="flex flex-1 mr-8 w-full">
+            <Project
+              src="project3.mp4"
+              title="MovieCloud"
+              description="Web application that provides detailed information on movies and TV series, utilizing the OMDb API.The platform ensures a seamless and engaging UX/UI, enabling effortless exploration and discovery of cinematic content."
+              technologies={"React - CSS - HTML - API"}
+              liveUrl="https://github.com/GusTiavo123/moviecloud"
+              repoUrl="https://github.com/GusTiavo123/moviecloud"
+            />
+          </div>
+          <div className="flex flex-1 w-full flex-col items-center justify-center ml-8 bg-custom-box-dark rounded-xl">
+            <h2 className="text-3xl text-white font-semi-bold mb-4">
+              Coming Soon
+            </h2>
+            <p className="text-custom-gray text-md">
+              New projects ideas coming soon...
+            </p>
+          </div>
+        </div>
+      </FadeInWhenVisible>
     </section>
   );
 }
