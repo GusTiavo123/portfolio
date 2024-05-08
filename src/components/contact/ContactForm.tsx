4cd048680c93e06482297432c8ceb487315b2c87
GusTiavo123 <luagus0@gmail.com>
2024-05-08 00:46:23 -0300

    fixing contact

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -9,7 +9,7 @@ const ContactForm: React.FC = () => {
   };
 
   const openEmailClient = () => {
-    const email = "gusaramayo2010@hotmail.com"; 
+    const email = "gusaramayo2010@hotmail.com";
     if (email) {
       window.location.href = `mailto:${email}`;
     }
@@ -17,33 +17,26 @@ const ContactForm: React.FC = () => {
 
   return (
     <div
-      className="relative flex flex-col bg-custom-box-dark border-t-2 border-custom-box-border rounded-xl py-5 pl-10 md:pr-10 lg:pr-60 justify-between md:mr-8 lg:hover:scale-105 duration-300 transition-all lg:hover:shadow-md ease-out lg:hover:shadow-custom-green shadow-custom-green lg:shadow-none shadow-md cursor-pointer"
+      className="relative flex flex-col bg-custom-box-dark border-t-2 border-custom-box-border rounded-xl py-5 pl-4 sm:pl-10 md:pr-10 lg:pr-60 justify-between md:mr-8 lg:hover:scale-105 duration-300 transition-all lg:hover:shadow-md ease-out lg:hover:shadow-custom-green shadow-custom-green lg:shadow-none shadow-md cursor-pointer"
       onClick={openEmailClient}
     >
       <img
         src="svg/website.svg"
         alt="Your Description"
-        className="absolute right-3 h-5 w-5 md:top-5 md:right-4 md:h-8 md:w-8"
+        className="absolute top-2 right-3 h-6 w-6 md:top-5 md:right-4 md:h-8 md:w-8"
       />
       <span className="text-custom-gray text-xs md:text-base w-full">
         Let's work together!
       </span>
-
-      {isMobileScreenSize() ? (
-        <h3 className="text-white font-bold text-lg w-full bg-custom-box-dark rounded-xl cursor-text">
-          gusaramayo2010@hotmail.com
-        </h3>
-      ) : (
-        <input
-          ref={emailRef}
-          type="text"
-          defaultValue="gusaramayo2010@hotmail.com"
-          readOnly
-          className="text-white font-bold text-2xl w-full bg-custom-box-dark rounded-xl cursor-text object-fill"
-          onClick={(e) => e.stopPropagation()}
-          onFocus={(e) => e.currentTarget.select()}
-        />
-      )}
+      <input
+        ref={emailRef}
+        type="text"
+        defaultValue="gusaramayo2010@hotmail.com"
+        readOnly
+        className="text-white font-bold md:text-2xl text-lg w-full bg-custom-box-dark rounded-xl cursor-text object-fill"
+        onClick={(e) => e.stopPropagation()}
+        onFocus={(e) => e.currentTarget.select()}
+      />
     </div>
   );
 };
