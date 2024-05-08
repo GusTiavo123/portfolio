bf8f74b19649ff573e891f665049c32923f79029
GusTiavo123 <luagus0@gmail.com>
2024-05-08 00:36:03 -0300

    chaging contact format

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -3,8 +3,13 @@ import { useRef } from "react";
 const ContactForm: React.FC = () => {
   const emailRef = useRef<HTMLInputElement>(null);
 
+  const isMobileScreenSize = () => {
+    const maxWidthForMobile = 768;
+    return window.innerWidth <= maxWidthForMobile;
+  };
+
   const openEmailClient = () => {
-    const email = emailRef.current?.value;
+    const email = "gusaramayo2010@hotmail.com"; 
     if (email) {
       window.location.href = `mailto:${email}`;
     }
@@ -18,20 +23,27 @@ const ContactForm: React.FC = () => {
       <img
         src="svg/website.svg"
         alt="Your Description"
-        className="absolute top-5 right-3 h-6 w-6 md:top-5 md:right-4 md:h-8 md:w-8"
+        className="absolute right-3 h-5 w-5 md:top-5 md:right-4 md:h-8 md:w-8"
       />
       <span className="text-custom-gray text-xs md:text-base w-full">
         Let's work together!
       </span>
-      <input
-        ref={emailRef}
-        type="text"
-        defaultValue="gusaramayo2010@hotmail.com"
-        readOnly
-        className="text-white font-bold md:text-2xl text-lg w-full bg-custom-box-dark rounded-xl cursor-text object-fil"
-        onClick={(e) => e.stopPropagation()}
-        onFocus={(e) => e.currentTarget.select()}
-      />
+
+      {isMobileScreenSize() ? (
+        <h3 className="text-white font-bold text-lg w-full bg-custom-box-dark rounded-xl cursor-text">
+          gusaramayo2010@hotmail.com
+        </h3>
+      ) : (
+        <input
+          ref={emailRef}
+          type="text"
+          defaultValue="gusaramayo2010@hotmail.com"
+          readOnly
+          className="text-white font-bold text-2xl w-full bg-custom-box-dark rounded-xl cursor-text object-fill"
+          onClick={(e) => e.stopPropagation()}
+          onFocus={(e) => e.currentTarget.select()}
+        />
+      )}
     </div>
   );
 };
